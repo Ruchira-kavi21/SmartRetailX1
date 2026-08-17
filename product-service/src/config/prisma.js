1c788b7decb94bf6dd6c97e61ae973fcdf2ae279
Ruchira Kaveesha <ruchirakaveesha2020@gmail.com>
2026-08-17 14:55:48 +0530

    Dockerize product service

--- product-service/src/config/prisma.js
+++ product-service/src/config/prisma.js
@@ -2,13 +2,17 @@ const { PrismaClient } = require("@prisma/client");
 const { PrismaMariaDb } = require("@prisma/adapter-mariadb");
 
 const adapter = new PrismaMariaDb({
-  host: "localhost",
-  port: 3307,
-  user: "root",
-  password: "rootpassword",
-  database: "product_db",
+    host: process.env.MYSQL_HOST || "mysql",
+    port: Number(process.env.MYSQL_PORT) || 3306,
+    user: process.env.MYSQL_USER || "root",
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE || "product_db",
+    connectionLimit: 5,
+    allowPublicKeyRetrieval: true
 });
 
-const prisma = new PrismaClient({ adapter });
+const prisma = new PrismaClient({
+    adapter
+});
 
 module.exports = prisma;
